e023bed8e152c5ec08019e692a07171eb9867556
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:07:59 +0300

    2020:12:04 00:07

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-screensaver',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/.screensaver-name.applescript','scripts/.screensaver-names.applescript','scripts/screensaver','scripts/screensaver-name','scripts/screensaver-names','scripts/screensaver-pid','scripts/screensaver-start','scripts/screensaver-stop','scripts/screensaver-wait']
